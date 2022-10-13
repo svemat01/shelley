c433a355cd875b4af4b0e24f540bbccd708da684
Svemat01 <jakob.helgesson@gmail.com>
2022-10-13 13:18:14 +0200

    Improve validator

--- backend/pkg/validator.go
+++ backend/pkg/validator.go
@@ -2,15 +2,32 @@ package pkg
 
 import (
 	"github.com/go-playground/validator/v10"
+	"log"
+	"reflect"
+	"strings"
 )
 
 type ErrorResponse struct {
-	FailedField string
-	Tag         string
-	Value       string
+	Field  string `json:"field"`
+	Value  string `json:"value"`
+	Reason string `json:"reason"`
 }
 
 var validate = validator.New()
+
+func init() {
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+
+		if name == "-" {
+			return ""
+		}
+
+		return name
+	})
+
+	log.Println("Validator initialized")
+}
 
 func ValidateStruct(data interface{}) []*ErrorResponse {
 	var errors []*ErrorResponse
@@ -18,9 +35,9 @@ func ValidateStruct(data interface{}) []*ErrorResponse {
 	if err != nil {
 		for _, err := range err.(validator.ValidationErrors) {
 			var element ErrorResponse
-			element.FailedField = err.Field()
-			element.Tag = err.Tag()
-			element.Value = err.Param()
+			element.Field = err.Field()
+			element.Reason = err.Tag()
+			element.Value = err.Value().(string)
 			errors = append(errors, &element)
 		}
 	}
