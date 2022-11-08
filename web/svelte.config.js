73acf1b8b2e5c20edcb9b07e3a1d6339ee4f0f91
Svemat01 <jakob.helgesson@gmail.com>
2022-11-08 10:08:12 +0100

    Init web frontend

---
+++ web/svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true,
+			fallback: '200.html'
+		}),
+		trailingSlash: 'always'
+	}
+};
+
+export default config;
